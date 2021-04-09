4dce81b6bd4c7184235b405b13cdfdc95e5f71bc
Atenadru <yorman@outlook.com>
2021-04-09 14:14:33 -0500

    fix  meta viewport

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <h1>este es el contenido</h1>
     </div>
