e78f92d076a3d895ef065abe550a1221cbc67246
Atenadru <yorman@outlook.com>
2021-04-09 14:03:04 -0500

    add meta viewport

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <h1>este es el contenido</h1>
     </div>
   );
